ca8e38933ec7390ccf44c8b0a683f56752bda81f
asilcox <asilcox123@gmail.com>
2026-09-01 14:58:23 -0400

    Create Window class

---
+++ FightingGameEngine/Window.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Windows.h>
+#include <optional>
+
+class Window
+{
+public:
+	Window(int width, int height, const char* name);
+	~Window();
+	static std::optional<int> ProcessMessages();
+private:
+	static LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+private:
+	int width;
+	int height;
+	HWND hWnd;
+	const char* windowName = "Fighting Game Engine";
+	HINSTANCE hInstance;
+};
