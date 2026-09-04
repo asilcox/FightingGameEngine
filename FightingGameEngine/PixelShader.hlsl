8d5962c7258b7329e712ba890d823bc177ab30be
asilcox <asilcox123@gmail.com>
2026-09-04 15:38:37 -0400

    Update pixel shader for directional lighting

--- FightingGameEngine/PixelShader.hlsl
+++ FightingGameEngine/PixelShader.hlsl
@@ -1,4 +1,23 @@
-float4 main(float3 normal : NORMAL) : SV_TARGET
+cbuffer LightCBuf
 {
-    return float4(normal, 1.0f);
+    float3 lightDir;
+    float3 lightPos;
+    float3 ambient;
+    float3 diffuseColor;
+    float diffuseIntensity;
+    float attConst;
+    float attLin;
+    float attQuad;
+};
+
+float4 main(float3 worldPos : POSITION, float3 n : NORMAL) : SV_TARGET
+{
+    const float3 vToL = lightDir * (lightPos - worldPos);
+    const float distToL = length(vToL);
+    const float3 dirToL = vToL / distToL;
+
+    const float fAngularAtt = saturate(dot(n, vToL));
+    const float3 diffuse = diffuseColor * diffuseIntensity * fAngularAtt * max(0.0f, dot(dirToL, n));
+
+    return float4(diffuse, 1.0f);
 }
