76be7a6f7bf882cc4910951630937581cf3bd7f5
asilcox <asilcox123@gmail.com>
2026-09-04 15:36:11 -0400

    Create directional light constant buffer struct in engine math

--- FightingGameEngine/EngineMath.h
+++ FightingGameEngine/EngineMath.h
@@ -30,3 +30,15 @@ struct Vertex
 		float v;
 	} texCoord;
 };
+
+struct DirectionalLightCBuf
+{
+	alignas(16) DirectX::XMFLOAT3 dir;
+	alignas(16) DirectX::XMFLOAT3 pos;
+	alignas(16) DirectX::XMFLOAT3 ambient;
+	alignas(16) DirectX::XMFLOAT3 diffuseColor;
+	float diffuseIntensity;
+	float attConst;
+	float attLin;
+	float attQuad;
+};
